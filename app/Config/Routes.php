6dfa34c99289f33c94ac24d80f06eb7c529367b5
Priyansh Jain <me.priyanshjain@gmail.com>
2023-08-16 16:34:42 +0530

    Credential : Login Check route setup

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,7 +30,7 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Credentials::Login');
-$routes->post('/Credentials/login_check', 'Credentials::login_check');
+$routes->post('Credentials/login_check', 'Credentials::login_check');
 
 
 /*
