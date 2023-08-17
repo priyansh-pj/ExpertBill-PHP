40c4d8f50e4b1a9fd6d5f96357c7d0e758176c8e
Priyansh Jain <me.priyanshjain@gmail.com>
2023-08-17 13:03:50 +0530

    Credential : Login Check route setup

--- app/Controllers/Credentials.php
+++ app/Controllers/Credentials.php
@@ -13,29 +13,28 @@ class Credentials extends BaseController
     }
     public function Login()
     {
-
         return view('Credentials/login');
     }
 
     public function login_check()
     {
-        $encryption = \Config\Services::encryption();
-        var_dump($_POST);
+        var_dump("running");
         die();
-        $hash = ($this->credentials_model->get_info_username($_POST['username']))->password;
+        $encryption = \Config\Services::encryption();
+        $hash = ($this->credentials_model->get_hash($_POST['email']))->password;
         if (!empty($hash) && password_verify($_POST['password'], $hash)) {
-            $this->session->set(["password_check" => true, "username" => $_POST['username'], "uid" => ($this->credentials_model->get_uid($_POST['username'])[0]->uid), "profile" => ($this->credentials_model->get_profile($_POST['username']))[0]]);
+            $this->session->set(["password_check" => true, "profile" => ($this->credentials_model->get_profile($_POST['email']))]);
             return redirect()->to(base_url('databliss/organization_verify/' . $_POST['username']));
         } else {
             echo 'Password is incorrect';
         }
-
     }
     public function register()
     {
         $_POST['name'] = explode(' ', $_POST['name']);
         $_POST['password'] = password_hash($_POST['password'], PASSWORD_BCRYPT);
-
+        var_dump($_POST);
+        die();
         $this->credentials_model->register_user($_POST);
         $this->session->set(["password_check" => true, "username" => $_POST['username'], "uid" => ($this->credentials_model->get_uid($_POST['username'])[0]->uid), "role" => "", "profile" => ($this->credentials_model->get_profile($_POST['username']))[0]]);
         return redirect()->to(base_url('databliss/organization_verify/' . $_POST['username']));
--- app/Models/Credentials_model.php
+++ app/Models/Credentials_model.php
@@ -6,14 +6,14 @@ use CodeIgniter\Model;
 
 class Credentials_model extends Model
 {
-    public function get_info_username($username)
+    public function get_hash($email)
     {
-        $query = "SELECT password FROM user_credentials WHERE username = ? ";
-        return $this->db->query($query, [$username])->getResult()[0];
+        $query = "SELECT password FROM user_credentials WHERE email = ? ";
+        return $this->db->query($query, [$email])->getResult()[0];
     }
-    public function get_uid($username)
+    public function get_profile($email)
     {
-        $query = "SELECT uid FROM user_credentials WHERE username = ?";
-        return $this->db->query($query, [$username])->getResult();
+        $query = "SELECT uid,username,first_name,last_name,email,contact_no FROM user_credentials WHERE email = ?";
+        return $this->db->query($query, [$email])->getResult()[0];
     }
 }
