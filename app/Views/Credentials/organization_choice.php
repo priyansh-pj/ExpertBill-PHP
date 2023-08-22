02fbde80ba9b0a93da1c39b2a312078087c6334f
Priyansh Jain <me.priyanshjain@gmail.com>
2023-08-22 16:20:43 +0530

    Credentails: Organization select

--- app/Views/Credentials/organization_choice.php
+++ app/Views/Credentials/organization_choice.php
@@ -3,23 +3,40 @@
         <?php foreach ($organizations as $organization) : ?>
             <?php $count = 0 ?>
             <div class="col-md-3 grid-margin stretch-card" style="margin-bottom:1.3rem">
-                <div class="card">
-                    <div class="card-body" style="padding:1.7rem;">
-                        <div class="clearfix">
-                            <h3 class="page-title">
-                                <span class="page-title-icon bg-gradient-primary text-white me-2">
-                                    <i class="mdi mdi-home"></i>
-                                </span> <?= $organization->name ?>
-                            </h3>
+                <a href="<?= base_url('Organization/verify/' . $organization->id) ?>">
+                    <div class="card">
+                        <div class="card-body" style="padding:1.7rem;">
+                            <div class="clearfix">
+                                <h3 class="page-title">
+                                    <span class="page-title-icon bg-gradient-primary text-white me-2">
+                                        <i class="mdi mdi-bookmark-check"></i>
+                                    </span> <?= $organization->name ?>
+                                </h3>
+                            </div>
                         </div>
                     </div>
+                </a>
+            </div>
+            <?php $count++; ?>
+            <?php if ($count % 4 == 0) : ?>
+    </div>
+    <div class="row">
+    <?php endif; ?>
+<?php endforeach; ?>
+<div class="col-md-3 grid-margin stretch-card" style="margin-bottom:1.3rem">
+    <a href="<?= base_url('Organization/make') ?>">
+        <div class="card">
+            <div class="card-body" style="padding:1.7rem;">
+                <div class="clearfix">
+                    <h3 class="page-title">
+                        <span class="page-title-icon bg-gradient-primary text-white me-2">
+                            <i class="mdi mdi-library-plus"></i>
+                        </span> Create Organization
+                    </h3>
                 </div>
-                <?php $count++; ?>
-                <?php if ($count % 3 == 0) : ?>
             </div>
-            <div class="row">
-            <?php endif; ?>
-        <?php endforeach; ?>
-            </div>
-
-        <?php endif; ?>
+        </div>
+    </a>
+</div>
+    </div>
+<?php endif; ?>
