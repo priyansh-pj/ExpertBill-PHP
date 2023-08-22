cb8580fa9f95b564207e65ec7a0b89c150e000c9
Priyansh Jain <me.priyanshjain@gmail.com>
2023-08-22 16:11:18 +0530

    Credentails: Organization select

--- app/Views/Credentials/organization_choice.php
+++ app/Views/Credentials/organization_choice.php
@@ -2,25 +2,24 @@
     <div class="row">
         <?php foreach ($organizations as $organization) : ?>
             <?php $count = 0 ?>
-            <div class="col-md-4 grid-margin stretch-card" style="margin-bottom:1.3rem">
+            <div class="col-md-3 grid-margin stretch-card" style="margin-bottom:1.3rem">
                 <div class="card">
                     <div class="card-body" style="padding:1.7rem;">
                         <div class="clearfix">
-                            <h3>
+                            <h3 class="page-title">
                                 <span class="page-title-icon bg-gradient-primary text-white me-2">
                                     <i class="mdi mdi-home"></i>
-                                </span><?= $organization->name ?>
+                                </span> <?= $organization->name ?>
                             </h3>
                         </div>
                     </div>
                 </div>
+                <?php $count++; ?>
+                <?php if ($count % 3 == 0) : ?>
             </div>
-            <?php $count++; ?>
-            <?php if ($count % 3 == 0) : ?>
-    </div>
-    <div class="row">
-    <?php endif; ?>
-<?php endforeach; ?>
-    </div>
+            <div class="row">
+            <?php endif; ?>
+        <?php endforeach; ?>
+            </div>
 
-<?php endif; ?>
+        <?php endif; ?>
