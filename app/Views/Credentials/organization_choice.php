fc54fa94d0817092b556a17d4105c963a2635747
Priyansh Jain <me.priyanshjain@gmail.com>
2023-08-22 15:52:54 +0530

    Credentails: Organization select

--- app/Views/Credentials/organization_choice.php
+++ app/Views/Credentials/organization_choice.php
@@ -5,11 +5,11 @@
             <div class="col-md-4 grid-margin stretch-card" style="margin-bottom:1.3rem">
                 <div class="card">
                     <div class="card-body">
-                        <span class="page-title-icon bg-gradient-primary text-white me-2">
-                            <i class="mdi mdi-blur"></i>
-                        </span>
-                        <h4 class="card-title"><?= $organization->name ?></h4>
 
+                        <h4 class="card-title"><span class="page-title-icon bg-gradient-primary text-white me-2">
+                                <i class="mdi mdi-blur"></i>
+                            </span><?= $organization->name ?></h4>
+                        role
                     </div>
                 </div>
             </div>
