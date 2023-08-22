7537c99eba2449e77938774a59be3eed8c4e41e8
Priyansh Jain <me.priyanshjain@gmail.com>
2023-08-22 15:12:44 +0530

    Credentails: Update in header

--- app/Views/Templates/header.php
+++ app/Views/Templates/header.php
@@ -247,3 +247,15 @@
             <!-- Sidebar End -->
             <div class="main-panel">
                 <div class="content-wrapper">
+                    <div class="row">
+                        <div class="col-md-12 grid-margin stretch-card">
+                            <div class="card">
+                                <div class="card-body" style="padding:1.2rem;">
+                                    <div class="clearfix">
+                                        <h4 class="card-title float-left" style="margin-bottom: 0px;"><?= $title ?></h4>
+                                        <div id="visit-sale-chart-legend" class="rounded-legend legend-horizontal legend-top-right float-right"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
