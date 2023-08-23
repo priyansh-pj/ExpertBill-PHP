a35d8076559a18a50dff7a74fcb9e2b66d78b206
Priyansh Jain <me.priyanshjain@gmail.com>
2023-08-23 13:40:56 +0530

    Organization: Implementation of Search View

---
+++ app/Views/Organization/organization_search.php
@@ -0,0 +1,23 @@
+<div class="col-12 grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <form class="form-inline">
+                <div class="col-md-8">
+                    <div class="form-group">
+                        <label>Organizations</label>
+                        <select class="form-control">
+                            <option>America</option>
+                            <option>Italy</option>
+                            <option>Russia</option>
+                            <option>Britain</option>
+                        </select>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-8">
+            <button type="submit" class="btn btn-gradient-primary mb-2">Submit</button>
+        </div>
+
+    </div>
+</div>
+</div>
